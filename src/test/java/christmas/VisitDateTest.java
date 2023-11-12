5d3970d33fb99a9afec1ab53907a8a4634fefeaf
JAY-Winter <rubat0@naver.com>
2023-11-12 14:35:57 +0900

    test: 방문날짜 공백 테스트 케이스 추가

--- src/test/java/christmas/VisitDateTest.java
+++ src/test/java/christmas/VisitDateTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class VisitDateTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"-1", "0", "32", "100"})
+    @ValueSource(strings = {"-1", "0", "32", "100", "1  "})
     void 방문날짜는_1_이상_31_이하여야합니다(String value) {
         assertThatThrownBy(() -> new VisitDate(value))
             .isInstanceOf(IllegalArgumentException.class);
