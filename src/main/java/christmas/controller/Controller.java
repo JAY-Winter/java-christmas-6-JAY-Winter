6add2d608317c612f3dd6833cc339f760a31dfe9
JAY-Winter <rubat0@naver.com>
2023-11-15 22:19:02 +0900

    style: 필요없는 패키지 제거

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -11,7 +11,6 @@ import christmas.util.Retry;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
