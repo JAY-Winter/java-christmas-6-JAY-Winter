2e977c5b1ced5126bda7323e3692f0456493ebb4
JAY-Winter <rubat0@naver.com>
2023-11-15 22:56:24 +0900

    test: 방문날짜가 특별한 날인지

--- src/test/java/christmas/VisitDateTest.java
+++ src/test/java/christmas/VisitDateTest.java
@@ -1,6 +1,8 @@
 package christmas;
 
 import christmas.model.order.VisitDate;
+import christmas.model.payment.SpecialDays;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,5 +15,19 @@ class VisitDateTest {
     void 방문날짜는_1_이상_31_이하여야합니다(String value) {
         assertThatThrownBy(() -> new VisitDate(value))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "10", "17", "24", "25", "31"})
+    void 방문날짜가_특별한_날일_경우(String value) {
+        boolean result = SpecialDays.contains(new VisitDate(value));
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "4", "5", "30"})
+    void 방문날짜가_특별한_날이_아닌_경우(String value) {
+        boolean result = SpecialDays.contains(new VisitDate(value));
+        Assertions.assertThat(result).isFalse();
     }
 }
